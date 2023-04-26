3dd2a31b3ae84d29751f6f96fa2f53ead8cd331f
Joshua Hoeflich <joshuaharry411@gmail.com>
2023-04-26 10:15:20 -0500

    Rendering the page works as desired

--- src/InvoiceTableForm.tsx
+++ src/InvoiceTableForm.tsx
@@ -1,7 +1,21 @@
 import React from "react";
 import { Invoice } from "./api";
 import { useInvoices } from "./InvoiceApiProvider";
-import { Flex, Heading, Text } from "@chakra-ui/react";
+import {
+  Flex,
+  Heading,
+  Input,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+  Select,
+  Button,
+} from "@chakra-ui/react";
 
 interface InvoiceFormContext {
   form: Invoice[];
@@ -10,12 +24,50 @@ interface InvoiceFormContext {
 
 const InvoiceFormContext = React.createContext<InvoiceFormContext | null>(null);
 
+const STATUSES = [
+  { value: "draft", display: "Draft" },
+  { value: "outstanding", display: "Outstanding" },
+  { value: "paid", display: "Paid" },
+];
+
 const useInvoiceForm = (): InvoiceFormContext => {
   const ctx = React.useContext(InvoiceFormContext);
   if (ctx === null) {
     throw new Error("Please use useInvoiceForm inside of its provider.");
   }
   return ctx;
+};
+
+const InvoiceTableFormBody = () => {
+  const { form, setForm } = useInvoiceForm();
+  console.log(form);
+  return (
+    <>
+      {form.map((invoice, idx) => {
+        return (
+          <Tr key={invoice.id}>
+            <Td>{invoice.id}</Td>
+            <Td>
+              <Input value={invoice.name} />
+            </Td>
+            <Td>
+              <Select value={invoice.status}>
+                {STATUSES.map((status) => {
+                  return <option value={status.value}>{status.display}</option>;
+                })}
+              </Select>
+            </Td>
+            <Td>
+              <Input value={invoice.due_date} type="date" />
+            </Td>
+            <Td>
+              <Button>View Charges</Button>
+            </Td>
+          </Tr>
+        );
+      })}
+    </>
+  );
 };
 
 export const InvoiceTableForm = () => {
@@ -27,8 +79,33 @@ export const InvoiceTableForm = () => {
     <InvoiceFormContext.Provider
       value={{ form: formState, setForm: setFormState }}
     >
-      <Flex direction="column" alignItems="center" width="100%">
-        <Heading my="16px">Invoices</Heading>
+      <Flex m={4} direction="column" width="100%" maxWidth="1200px">
+        <Flex width="100%" justifyContent="center">
+          <Heading>Invoices</Heading>
+        </Flex>
+        <Flex my="16px">
+          <Button mr="16px" colorScheme="green">
+            Create
+          </Button>
+          <Button colorScheme="teal">Save</Button>
+        </Flex>
+        <TableContainer>
+          <Table variant="simple">
+            <TableCaption>Edit the entries in the table directly.</TableCaption>
+            <Thead>
+              <Tr>
+                <Th>ID</Th>
+                <Th>Name</Th>
+                <Th>Status</Th>
+                <Th>Due Date</Th>
+                <Th>Charges</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <InvoiceTableFormBody />
+            </Tbody>
+          </Table>
+        </TableContainer>
       </Flex>
     </InvoiceFormContext.Provider>
   );
